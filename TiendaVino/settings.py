7a2aae2011177a553f8c6348342835b6a59e3521
urabe153 <sj63035794@zegel.pe>
2024-06-08 00:14:34 -0500

    metodo de pago en 70%

--- TiendaVino/settings.py
+++ TiendaVino/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Modulos.Gestion',
-    'payment.apps.PaymentConfig'
+    'payment.apps.PaymentConfig',
+    'orders.app.OrdersConfing'
 ]
 
 MIDDLEWARE = [
