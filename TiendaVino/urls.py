dd50c46bcd37739b104d3aaa37375a2f817ac637
urabe153 <sj63035794@zegel.pe>
2024-06-10 22:21:01 -0500

    metodo de pago 70%

--- TiendaVino/urls.py
+++ TiendaVino/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Modulos.Gestion.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment-urls', namespace='payment')),
+    path('', include('Modulos.Gestion.urls', namespace='Modulos.Gestion')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)    
